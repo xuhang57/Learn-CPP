2664345178257f8bff54ea5c95306f44af7b7d6b
xuhang57 <xuh@bu.edu>
2018-01-29 21:21:49 -0500

    add triangle poem

---
+++ poem.cpp
@@ -0,0 +1,10 @@
+// Copyright 2018 Hang Xu and xuh@bu.edu
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hi," << "\n";
+  std::cout << "I like" << "\n";
+  std::cout << "playing basketball." << "\n";
+  return 0;
+}
